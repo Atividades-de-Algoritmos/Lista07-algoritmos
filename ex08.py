44ff83c5929c25d7e2fbfe2550640af0413abbab
Michel <106674127+MichelZero@users.noreply.github.com>
2022-06-26 09:13:07 -0300

    Create ex08.py

---
+++ ex08.py
@@ -0,0 +1,10 @@
+#
+#
+# autores:
+# Michel Silva
+#
+# data: 25/06/2022
+#
+# 8.	 Faça um programa que leia vários inteiros positivos e mostre, no final, a soma dos números pares e a soma
+# dos números ímpares. O programa de parar quando entrar um número maior que 1000.
+
