36bd724c9db4268242011e596c77011db9602625
Michel <106674127+MichelZero@users.noreply.github.com>
2022-06-26 10:15:29 -0300

    Update ex06.py

--- ex06.py
+++ ex06.py
@@ -5,10 +5,10 @@
 # 
 # data: 25/06/2022
 #
-# 6. Faça um programa algoritmo para exibir a tabuada de 0 a 9.
+# 6. Faça um programa algoritmo para exibir a tabuada de 1 a 10.
 
-for i in range(0, 10): # Laço de repetição que percorre os números de 0 a 9 (n+1) e armazena o valor na variável i
-  for j in range(0, 10): # Laço de repetição que percorre os números de 0 a 9 (n+1) e armazena o valor na variável j
+for i in range(1, 11): # Laço de repetição que percorre os números de 1 a 10 (n+1) e armazena o valor na variável i
+  for j in range(1, 11): # Laço de repetição que percorre os números de 1 a 10 (n+1) e armazena o valor na variável j
     print(f"{i} x {j} = {i * j}") # Imprime o resultado da multiplicação de i e j
 
 print("Fim do programa") # Informa ao usuário que o programa terminou
