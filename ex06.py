070bd3b9b0f9f049f6f1850106503d722749dac9
Michel <106674127+MichelZero@users.noreply.github.com>
2022-10-25 14:17:30 -0300

    up

--- ex06.py
+++ ex06.py
@@ -13,3 +13,28 @@ for i in range(1, 11): # Laço de repetição que percorre os números de 1 a 10
     print(f"{i} x {j} = {i * j}") # Imprime o resultado da multiplicação de i e j
 
 print("Fim do programa") # Informa ao usuário que o programa terminou
+
+
+print("----------------------------------------------------")
+# 6. Faça um programa que informado um valor e a operação,
+# calcule e
+# exibir a tabuada de 1 a 10, deste valor.
+
+# entrada de dados
+valor = int(input("informe um valor: "))
+op  = input("informe {+ , - , * , /}")
+
+for i in range(1, 11):
+  if op == '+':
+    print(f"{i}+{valor}={i+valor}")
+  elif op == '-':
+    print(f"{i}-{valor}={i-valor}") 
+  elif op == '*':
+    print(f"{i}*{valor}={i*valor}")
+  elif op == '/':
+    print(f"{valor}/{i}={valor/i}") 
+  else:
+    print("operação inexistente") 
+
+
+
